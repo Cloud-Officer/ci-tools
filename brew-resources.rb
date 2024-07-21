f2d8bb62e85ba894d14350c815867d040bbeb5f7
Yves Desgagné <yves@cloudofficer.ca>
2024-07-21 09:14:02 -0400

    Fixed brew-resources to handle multi platforms correctly (#98)

--- brew-resources.rb
+++ brew-resources.rb
@@ -10,20 +10,37 @@ begin
   lock_file = Bundler::LockfileParser.new(Bundler.read_file('Gemfile.lock'))
 
   lock_file.specs.each do |spec|
-    response = HTTParty.get("https://rubygems.org/gems/#{spec.name}-#{spec.version}.gem")
+    response = HTTParty.get("https://rubygems.org/gems/#{spec.full_name}.gem")
 
     raise(response.message) unless response.code == 200
 
-    if spec.name.include?('cocoapods')
-      puts('  if OS.mac?')
-      puts("    resource '#{spec.name}' do")
-      puts("      url 'https://rubygems.org/gems/#{spec.name}-#{spec.version}.gem'")
-      puts("      sha256 '#{Digest::SHA256.hexdigest(response.body)}'")
-      puts('    end')
-    else
+    if spec.platform == 'ruby'
       puts("  resource '#{spec.name}' do")
-      puts("    url 'https://rubygems.org/gems/#{spec.name}-#{spec.version}.gem'")
+      puts("    url 'https://rubygems.org/gems/#{spec.full_name}.gem'")
       puts("    sha256 '#{Digest::SHA256.hexdigest(response.body)}'")
+    else
+      os =
+        case spec.platform.os
+        when /darwin/
+          'on_macos'
+        else
+          'on_linux'
+        end
+
+      cpu =
+        if spec.platform.cpu == 'x86_64'
+          'on_intel'
+        else
+          'on_arm'
+        end
+
+      puts("  #{os} do")
+      puts("    #{cpu} do")
+      puts("      resource '#{spec.name}' do")
+      puts("        url 'https://rubygems.org/gems/#{spec.full_name}.gem'")
+      puts("        sha256 '#{Digest::SHA256.hexdigest(response.body)}'")
+      puts('      end')
+      puts('    end')
     end
 
     puts('  end')
